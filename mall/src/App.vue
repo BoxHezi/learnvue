54180402cd6eee0b8dd2c9eeae6d5a779109b633
bluu0010 <bluu0010@student.monash.edu>
2021-06-18 11:59:49 +1000

    format

--- mall/src/App.vue
+++ mall/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <div id="nav">
-      <router-link to="/">Home</router-link> |
+      <router-link to="/home">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
     <router-view />
@@ -9,7 +9,7 @@
 </template>
 
 <style>
-#app {
+/* #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
@@ -24,7 +24,7 @@
 #nav a {
   font-weight: bold;
   color: #2c3e50;
-}
+} */
 
 #nav a.router-link-exact-active {
   color: #42b983;
